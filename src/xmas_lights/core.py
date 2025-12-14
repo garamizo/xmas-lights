764187ba159d33293bd762d0077904af875b9e9c
Guilherme Aramizo Ribeiro <garamizo91@gmail.com>
2025-12-14 14:02:43 -0800

    refact calibration

--- src/xmas_lights/core.py
+++ src/xmas_lights/core.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import List, Tuple 
 import numpy as np
 
+
 def read_video(path: Path) -> List[np.ndarray]:
     # preload video at downsampled rate
     cap = cv2.VideoCapture(path)
@@ -38,7 +39,13 @@ def select_frames(frames: List[np.ndarray]) -> Tuple[List[np.ndarray], np.ndarra
     # return None, bright_norm, None
     assert len(idx_up) >= 2, "Could not find brightness transitions" 
     idx_up = idx_up[-2:]
+    
     return frames[idx_up[0]:idx_up[1]], bright_norm[idx_up[0]:idx_up[1]], bright_norm
+
+def init_camera_matrix(frame):
+    h, w, _ = frame.shape
+    f = 0.6 * h
+    return np.array([[f, 0, w/2], [0, f, h/2], [0, 0, 1]], np.float32)
 
 
 def filter_frames(frames: List[np.ndarray], num_flashes: int) -> Tuple[List[np.ndarray], np.ndarray]:
@@ -58,6 +65,7 @@ def filter_frames(frames: List[np.ndarray], num_flashes: int) -> Tuple[List[np.n
         rng = range(i*len_batch + 2, (i+1)*len_batch - 2)
         frames_filt.append(np.mean(frames_np[rng], axis=0).astype(np.uint8))
 
+    # TODO Should off state frame (last) be included here?
     im_std = np.max(np.std(frames_filt, axis=0), axis=-1)
     thresh = np.mean(np.percentile(im_std, [50, 100]))
     fg = im_std > thresh 
@@ -126,54 +134,3 @@ def xy_from_image(im_id, fg):
 
     return xys[inliers], ids[inliers], np.asarray(areas)[inliers]
 
-
-
-def process_video(self, videoFile):
-
-    FLASH_PERIOD, FLASH_QTY = self.flashPeriod, self.flashQty
-
-    # preload video at downsampled rate
-    cap = cv2.VideoCapture(videoFile)
-    FPS = cap.get(cv2.CAP_PROP_FPS)
-    CAP_PAUSE = 3  # downsample
-
-    bright, frames, ftime, time = [], [], [], []
-    count = -1
-    while True:
-        count += 1
-        ret, frame = cap.read()
-        if ret == False:
-            break
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-        bright.append(np.mean(hsv[:,:,2]))
-        time.append(count / FPS)
-        
-        if count % CAP_PAUSE == 0:
-            frames.append(frame)
-            ftime.append(time[-1])
-
-    bright, time, ftime = np.array(bright), np.array(time), np.array(ftime)
-    bright = signal.detrend(bright)
-
-    # segmenting frames of interest
-    tLastCycle = time[-1] - FLASH_PERIOD*FLASH_QTY
-    DIFF_THRESH = (np.percentile(bright[time > tLastCycle], 200.0/FLASH_QTY) +
-                    np.percentile(bright[time > tLastCycle], 50.0/FLASH_QTY)) / 2.0
-    # find center of last off-region
-    nStep = int(np.round(FLASH_PERIOD * FPS))
-    offRegion = (bright < DIFF_THRESH)# & (time > tLastCycle)
-    offEndInv = len(offRegion) - np.argmax(offRegion[::-1]) - int(nStep * (FLASH_QTY + 1.5)) # last occurrence
-
-    rows = offEndInv + np.arange(FLASH_QTY + 1) * nStep
-    self.frames = [frames[np.argmin(np.abs(ftime - t))] for t in time[rows]]
-    self.bg = self.frames.pop(0)
-
-    plt.plot(time, bright, '.-');
-    plt.plot(ftime, bright[np.isin(time, ftime)], 's');
-    # plt.plot(flashTime, [bright[np.argmin(np.abs(time - t))] for t in flashTime], '^');
-    for t in time[rows]:
-        plt.axvline(t, color='g', linestyle=':')
-    plt.axhline(DIFF_THRESH, color='r', linestyle='--');
-    plt.xlabel('time [s]'), plt.ylabel('brightness [0-255]'), plt.title('Frame Selection')
